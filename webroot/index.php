0df65fb60d3f07faf2f1e35e4d5f0259680b0283
Brian Porter <beporter@users.sourceforge.net>
2014-02-13 10:14:21 -0600

    Updated bootstrap files to reference composer-install Cake core.

--- webroot/index.php
+++ webroot/index.php
@@ -52,7 +52,7 @@ if (!defined('APP_DIR')) {
  * The following line differs from its sibling
  * /app/webroot/index.php
  */
-define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . APP_DIR . DS . 'Lib');
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . APP_DIR . '/Vendor/pear-pear.cakephp.org/CakePHP');
 
 /**
  * Editing below this line should NOT be necessary.
