84b814099421de689cbb68d9db7dd7abde84b37c
Brian Porter <beporter@users.sourceforge.net>
2014-07-09 12:08:47 -0500

    Adding back in necessary APP_DIR to core path.

--- webroot/test.php
+++ webroot/test.php
@@ -50,7 +50,7 @@ if (!defined('APP_DIR')) {
  * The following line differs from its sibling
  * /app/webroot/test.php
  */
-define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . APP_DIR . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS . 'lib');
 
 /**
  * Editing below this line should not be necessary.
