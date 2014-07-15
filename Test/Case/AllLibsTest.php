d5f4b63e5f63e1d93f18189d487dc5bdd3525870
Brian Porter <beporter@users.sourceforge.net>
2014-07-15 11:56:38 -0500

    Makes Lib test suite recrusive.

--- Test/Case/AllLibsTest.php
+++ Test/Case/AllLibsTest.php
@@ -5,7 +5,7 @@
 class AllLibsTest extends PHPUnit_Framework_TestSuite {
 	public static function suite() {
 		$suite = new CakeTestSuite('All Lib Tests');
-		$suite->addTestDirectory(dirname(__FILE__) . '/Lib/');
+		$suite->addTestDirectoryRecursive(dirname(__FILE__) . '/Lib/');
 		return $suite;
 	}
 }
