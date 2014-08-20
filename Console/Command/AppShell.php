1502d9ad237038066adbdc41d57aa9d44e6b8b13
Brian Porter <beporter@users.sourceforge.net>
2014-08-20 16:34:06 -0500

    Imported some handy shells.

--- Console/Command/AppShell.php
+++ Console/Command/AppShell.php
@@ -1,18 +1,7 @@
 <?php
 /**
- * AppShell file
+ * AppShell Parent class for all application Shells.
  *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 2.0
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 App::uses('Shell', 'Console');
@@ -23,8 +12,27 @@ App::uses('Shell', 'Console');
  * Add your application-wide methods in the class below, your shells
  * will inherit them.
  *
- * @package       app.Console.Command
  */
 class AppShell extends Shell {
 
+	/**
+	 * _out
+	 *
+	 * Wrapper around Shell::out() to provide consistent formatting.
+	 *
+	 * @access	protected
+	 * @param	string		$msg	The message to be sent to the console.
+	 * @param	string		$style	The Shell output style (''/error/warning/info/comment/question) to use for the message.
+	 * @param	integer		$level	The Shell::QUIET/NORMAL/VERBOSE output level.
+	 * @return integer|boolean Returns the number of bytes returned from writing to stdout.
+	 */
+	protected function _out($msg, $style = 'error', $level = Shell::NORMAL) {
+		if (empty($style)) {
+			$format = '%1$s';
+		} else {
+			$format = '<%2$s>%1$s</%2$s>';
+		}
+		$msg = sprintf($format, $msg, $style);
+		return $this->out($msg, 1, $level);
+	}
 }
---
+++ Console/Command/PasswordGenShell.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * PasswordGenShell
+ */
+
+App::uses('AppShell', 'Console/Command');
+App::uses('Security', 'Utility');
+
+/**
+ * Provides a command line interface to generate new passwords and UUIDs
+ * for manually inserting User records into the database. Designed to hook
+ * into the User model for consistent hashing, if the proper methods are
+ * available.
+ */
+class PasswordGenShell extends Shell {
+
+	/**
+	 * Models to load.
+	 *
+	 * @var array
+	 */
+	public $uses = array();
+
+	/**
+	 * Generate a UUID and a new random password for use when creating a new
+	 * User model account by hand.
+	 *
+	 * @access	public
+	 * @return	void.
+	 */
+	public function main() {
+		$plaintext = $this->generatePassword(14);
+		if (isset($this->args[0])) {
+			$plaintext = $this->args[0];
+		} elseif (!$this->params['random']) {
+			$input = $this->in('Please enter a new password (leave blank for random): ');
+			if (strlen($input) !== 0) {
+				$plaintext = $input;
+			}
+		}
+
+		$this->out('                       New UUID: ' . String::uuid());
+		$this->out('    New password (send to user): ' . $plaintext);
+		$this->out('Encrypted password (save to DB): ' . $this->hashPassword($plaintext));
+	}
+
+	/**
+	 * Check if the host app has a User->hashPassword($len) method defined,
+	 * and use that if present. Otherwise call our own implementation.
+	 *
+	 * @access	public
+	 * @param	string	$plaintext	The plaintext to hash.
+	 * @return	string				Returns the result of User->hashPassword() if available, otherwise Security::hash('blowfish').
+	 */
+	public function hashPassword($plaintext) {
+		$User = ClassRegistry::init('User');
+		if ($this->isActualClassMethod('hashPassword', $User)) {
+			return $User->hashPassword($plaintext);
+		}
+		return Security::hash($plaintext, 'blowfish');
+	}
+
+	/**
+	 * Check if the host app has a User->randomPassword($len) method defined,
+	 * and use that if present. Otherwise call our own implementation.
+	 *
+	 * @access	public
+	 * @param	integer	$len	The length of the requested string. Default = 12.
+	 * @return	string			Returns the result of User->randomPassword() if available, otherwise ::randomPassword().
+	 */
+	public function generatePassword($len = 12) {
+		$User = ClassRegistry::init('User');
+		if ($this->isActualClassMethod('randomPassword', $User)) {
+			echo 'using User->random';
+			return $User->randomPassword($len);
+		}
+		return $this->randomPassword($len);
+	}
+
+	/**
+	 * Generate a new pseudo-random password of given $len and return it as
+	 * a string.
+	 *
+	 * @access	public
+	 * @param	integer	$len	The length of the requested string. Default = 12.
+	 * @return	string			Returns a PSEUDO-random (NOT cryptogeraphically secure!) string of length = $len (max 64 chars).
+	 */
+	public function randomPassword($len = 12) {
+		return substr(base64_encode(uniqid(mt_rand(), true)), 0, min($len, 64));
+	}
+
+	/**
+	 * Confirm that a method is actually defined (and not shadowed by
+	 * `__call()` for the given object.)
+	 *
+	 * @access	protected
+	 * @param	string	$method	The name of the method to check.
+	 * @param	object	$obje	The instantiated object to check.
+	 * @return	boolean			True if the named method exists (not via __call()), false otherwise.
+	 */
+	protected function isActualClassMethod($method, $obj) {
+		return (
+			in_array($method, get_class_methods($obj))
+			&& is_callable(array($obj, $method))
+		);
+	}
+
+	/**
+	 * getOptionParser
+	 *
+	 * Processing command line options.
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	public function getOptionParser() {
+		$parser = parent::getOptionParser();
+		$parser
+			->addArgument('plaintext', array(
+				'help' => 'Optional plaintext for the new password. INSECURE. Takes precedence over the -r option.',
+				'required' => false,
+			))
+			->addOption('random', array(
+				'short' => 'r',
+				'boolean' => true,
+				'help' => __('Generate a random password automatically. (Do not prompt.)')
+			))
+			->description(__('Generates a UUID and a new random password for use when creating a new User model account by hand.'));
+		return $parser;
+	}
+}
