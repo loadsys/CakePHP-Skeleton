8e60c7b50087cba0ef382f41225ec90d3729c0d9
Brian Porter <beporter@users.sourceforge.net>
2015-06-30 16:40:23 -0500

    Figured out how to run Composer plugin tests.
    `vendor/bin/phpunit --bootstrap=tests/skeleton-bootstrap.php --coverage-html=tmp/coverage/html/ tests/TestCase/Console/`

---
+++ tests/skeleton-bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Test runner bootstrap.
+ *
+ * Add additional configuration/setup your application needs when running
+ * unit tests in this file.
+ */
+
+require dirname(__DIR__) . '/config/paths.php';
+
+// Use composer to load the autoloader.
+require ROOT . DS . 'vendor' . DS . 'autoload.php';
