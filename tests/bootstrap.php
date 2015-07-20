06b639420802e74afa36f7e1c7739d8a2c9583b2
Brian Porter <beporter@users.sourceforge.net>
2015-07-20 16:32:17 -0500

    Always clear caches before running tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,10 @@
  * unit tests in this file.
  */
 require dirname(__DIR__) . '/config/bootstrap.php';
+
+// Wipe out any accumulated caches before running tests.
+foreach(\Cake\Cache\Cache::configured() as $key) {
+	\Cake\Cache\Cache::clear(false, $key);
+	echo "Cleared cache: $key\n";
+}
+echo PHP_EOL;
