1c8275200714b0f467513d0e3327df8067d6346c
Brian Porter <beporter@users.sourceforge.net>
2015-08-16 14:15:41 -0500

    Switch to Inflected Routes by default.
    This will be the Cake 3.1 default anyway.

--- config/routes.php
+++ config/routes.php
@@ -70,7 +70,7 @@ Router::scope('/', function ($routes) {
 	 * You can remove these routes once you've connected the
 	 * routes you want in your application.
 	 */
-	$routes->fallbacks('InflectedRoute');
+	$routes->fallbacks('DashedRoute');
 });
 
 /**
