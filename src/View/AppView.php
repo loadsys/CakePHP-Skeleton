eff70470138a70335862400b83b65c5e034f0484
Justin Yost <justin@loadsys.com>
2015-09-03 13:31:34 -0700

    Updated AppView Class and Test Class
    Signed-off-by: Justin Yost <justin@loadsys.com>

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -1,25 +1,15 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link      http://cakephp.org CakePHP(tm) Project
- * @since     3.0.0
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * AppView class used for display all normal view instances.
  */
 namespace App\View;
 
 use Cake\View\View;
 
 /**
- * App View class
+ * \App\View\AppView
  */
 class AppView extends View {
-
 	/**
 	 * Initialization hook method.
 	 *
@@ -30,5 +20,7 @@ class AppView extends View {
 	 */
 	public function initialize() {
 		$this->loadHelper('Html');
+		$this->loadHelper('Form');
+		$this->loadHelper('Flash');
 	}
 }
